83295bd2139b2582e6a0cbfd597701802588304a
Akshay-Rahul <akshayrahul14@gmail.com>
2024-06-16 23:40:52 +0530

    update

--- src/pages/RegAim.js
+++ src/pages/RegAim.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
-import "../App.css";
 import gunFire from "../assets/9mm.mp3";
 import ScoreCard from "../components/ScoreCard/ScoreCard";
 import Timer from "../components/ScoreCard/Timer";
 import TargetRegAim from "../components/Target/TargetRegAim";
 import Result from "./Result";
 
-function RegAim() {
+function RegAim({ goHome, updateHighScore }) {
   const [score, setScore] = useState(0);
+
   const fire = () => {
     new Audio(gunFire).play();
+    setScore(score + 1); // Increment score on each click
   };
 
   const [countdown, setCountdown] = useState(15);
   const [gameOver, setGameOver] = useState(false);
 
+  // Effect to handle countdown and game over logic
   useEffect(() => {
     if (countdown === 0) {
       setGameOver(true);
@@ -26,10 +28,19 @@ function RegAim() {
     }
   }, [countdown]);
 
+  // Effect to update high score in localStorage when score changes
+  useEffect(() => {
+    const storedHighScore = localStorage.getItem("highScore");
+    const currentHighScore = storedHighScore ? parseInt(storedHighScore) : 0;
+    if (score > currentHighScore) {
+      updateHighScore(score); // Update high score in App.js
+    }
+  }, [score, updateHighScore]);
+
   return (
     <>
       {gameOver ? (
-        <Result score={score} />
+        <Result score={score} goHome={goHome} />
       ) : (
         <div className="container" onClick={fire}>
           <ScoreCard score={score} />
